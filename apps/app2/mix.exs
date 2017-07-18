e9e2de2c799cb6e0c3aa641dd1bd8009a845d3fe
Derek Hubbard <derekehubbard@gmail.com>
2017-07-18 10:55:47 -0400

    Changes Default Ports for App2
    The default ports for phoenix are 4000 for dev and 4001 for test.
    Updating these default ports in App2 to 4010 and 4011, respectively, so
    they don't collide with the ports used by App1.

--- apps/app2/mix.exs
+++ apps/app2/mix.exs
@@ -3,7 +3,7 @@ defmodule App2.Mixfile do
 
   def project do
     [app: :app2,
-     version: "0.0.1",
+     version: "0.0.2",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
