6d29133c5e679da902a5153ddceff6b0d6b5f3df
Derek Hubbard <derekehubbard@gmail.com>
2017-07-18 10:19:08 -0400

    Adds Distillery and Edeliver Dependencies
    Adds the distillery and edeliver dependencies to the umbrella project
    configuration.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule UmbrellaTest.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:distillery, "~> 1.4"},
+      {:edeliver, "~> 1.4"}
+    ]
   end
 end
