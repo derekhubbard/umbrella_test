17429292287c977f4fa729408ce021a15da7a711
Derek Hubbard <derekehubbard@gmail.com>
2017-07-18 13:31:24 -0400

    Tries a Different Approach for Compiling Assets

--- apps/app1/mix.exs
+++ apps/app1/mix.exs
@@ -3,7 +3,7 @@ defmodule App1.Mixfile do
 
   def project do
     [app: :app1,
-     version: "0.0.2",
+     version: "0.0.3",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
--- rel/config.exs
+++ rel/config.exs
@@ -41,7 +41,7 @@ end
 # will be used by default
 
 release :umbrella_test do
-  set version: "0.1.5"
+  set version: "0.1.6"
   set applications: [
     :runtime_tools,
     :app1,
